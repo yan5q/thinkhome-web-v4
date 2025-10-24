9c5366e18e6c21e443724da8569ad16a007ddb8e
Ondraaasek <ondrej.jansa015@gmail.com>
2025-10-24 22:25:45 +0200

    Enhance About component layout and content: Adjusted padding, added descriptive subheadings, and included a call-to-action for additional services.

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -5,12 +5,14 @@ import Link from "next/link";
 export default function About() {
     return (
         <div>
-            <div className="flex flex-col items-center justify-center justify-self-center p-10 w-[40vw]">
+            <div className="flex flex-col items-center justify-center justify-self-center p-10 pb-0 w-[40vw]">
                 <h1 className="text-3xl font-bold">Co vlastně děláme?</h1>
+                <h2 className="text-lg text-muted-foreground">Různé IT řešení, jako:</h2>
             </div>
             <div className="cursor-pointer">
                 <Link href="/sluzby">
                     <ScrollVelocity velocity={20} texts={["| Weby na zakázku | Kyberbezpečnost | AI | Cloud", "| IT správa | Repasování | Sítě"]} />
+               <h2 className="text-lg text-muted-foreground text-center">A další... klikněte pro více.</h2>
                 </Link>
             </div>
             <div className="flex flex-col items-center justify-center justify-self-center p-10 w-[40vw]">
